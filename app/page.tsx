84e7175166c981c20298692419c8df9ff838735c
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-16 13:36:06 -0300

    Verificação de log
    Usando função assíncrona para verificação de cookies, caso o usuário esteja logado a pagina mostrara uma área diferente em pages.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
-import Image from 'next/image'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
+import { RedirectType, redirect } from "next/navigation"
 
-export default function Home() {
+export default async function Home() {
+  let loggdIn = false
+  try{
+    const supabase = createServerComponentClient({cookies})
+    const { 
+      data: {session}, 
+     } = await supabase.auth.getSession()
+    if (session) loggdIn = true;
+    //if(session) redirect("/use-app")
+
+  }catch(error){
+    console.log("Home", error)
+  }finally{
+    if(loggdIn) redirect("/use-app", RedirectType.replace)
+  }
   return (
     <div className='text-lime-700 font-extrabold'>Home</div>
   )
---
+++ app/user-app/page.tsx
@@ -0,0 +1,7 @@
+export default async function UserApp(){
+    return(
+        <div>
+            Logado
+        </div>
+    )
+}
