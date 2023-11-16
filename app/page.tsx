e91bf39319ba91e486bfe5737f189d5b32cb13c9
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-16 14:37:45 -0300

    tabs de login
    Iniciando ajustes nos tabs de login.

--- app/page.tsx
+++ app/page.tsx
@@ -39,59 +39,19 @@ export default async function Home() {
   return (
     <div className="flex flex-col h-screen w-full justify-center items-center">
 
-<Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
+<Tabs defaultValue="create-account" className="w-[400px]
+  border rounded-md pb-4 shadow-xl">
+      <TabsList className=" flex justify-around
+        items-center  h-14">
+        <TabsTrigger value="create-account"
+        className="transition-all delay-150">Account</TabsTrigger>
+        <TabsTrigger value="login"
+        className="transition-all delay-150">login</TabsTrigger>
       </TabsList>
-      <TabsContent value="account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
+      <TabsContent value="create-account">
       </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
+      <TabsContent value="login"></TabsContent>
     </Tabs>
-
-    </div>
+  </div>
   )
 }
