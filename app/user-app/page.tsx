6aef6d4280c074c9d31f6fae0709962d10d1ea52
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-23 14:51:30 -0300

    user-app page
    Pagina do usuario com redirecionamento para a home caso o usuario nao esteja logado.

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -1,4 +1,26 @@
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { RedirectType } from "next/dist/client/components/redirect"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
+
 export default async function UserApp(){
+    let loggdIn = false
+  try{
+    const supabase = createServerComponentClient({cookies})
+    const { 
+      data: {session}, 
+     } = await supabase.auth.getSession()
+    if (session) loggdIn = true;
+    //if(session) redirect("/use-app")
+
+  }catch(error){
+    console.log("UserApp", error)
+  }finally{
+    if(!loggdIn) redirect("/", RedirectType.replace)
+  }
+
+
     return(
         <div>
             Logado
