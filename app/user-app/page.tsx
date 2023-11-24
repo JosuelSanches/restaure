1d88f5e524686ae5bd459d601a2a1bc9a97216ef
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-24 14:53:40 -0300

    sidebar
    configurando a side-bar, removendo itens desnecessarios e modificando a adaptacao da pagina na tela do usuario.

--- app/user-app/page.tsx
+++ app/user-app/page.tsx
@@ -30,8 +30,8 @@ export default async function UserApp(){
         <UserAppHeader/>
         <div className="border-t">
           <div className="bg-background">
-            <div className="grid lg:grid-cols-5">
-                <Sidebar/>
+            <div className="grid md:grid-cols-5">
+                <Sidebar className="hidden lg:block"/>
               <div className="col-span-3 lg:col-span-4 lg:border-l">
                 <div className="h-full px-4 py-6 lg:px-8">
                   {/* <Tabs defaultValue="music" className="h-full space-y-6">
--- components/user-app/user-app-sidebar.tsx
+++ components/user-app/user-app-sidebar.tsx
@@ -15,22 +15,7 @@ export function Sidebar({ className }: SidebarProps) {
             Discover
           </h2>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
-              </svg>
-              Listen Now
-            </Button>
+           
             <Button variant="ghost" className="w-full justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -66,7 +51,7 @@ export function Sidebar({ className }: SidebarProps) {
                 <path d="M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5" />
                 <path d="M19.1 4.9C23 8.8 23 15.1 19.1 19" />
               </svg>
-              Radio
+              Favorites
             </Button>
           </div>
         </div>
@@ -161,11 +146,6 @@ export function Sidebar({ className }: SidebarProps) {
               Albums
             </Button>
           </div>
-        </div>
-        <div className="py-2">
-          <h2 className="relative px-7 text-lg font-semibold tracking-tight">
-            Playlists
-          </h2>
         </div>
       </div>
     </div>
